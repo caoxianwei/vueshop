4e15a2b6fd8dd91d901867b5ae52bbc62adf2f54
caoxianwei <1756862146@qq.com>
2017-10-29 23:23:56 +0800

    init layout

---
+++ src/components/layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="app-header">header</div>
+    <div class="app-content">content</div>
+    <div class="app-footer">footer</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        msg: 'ffff',
+        price: 5
+      }
+    }
+  }
+</script>
+<style>
+</style>
